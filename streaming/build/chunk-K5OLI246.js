9094876c7684726952820c9c9d101aa086ece530
victor <55814035+vniox@users.noreply.github.com>
2026-03-22 15:35:03 -0500

    Add files via upload

---
+++ streaming/build/chunk-K5OLI246.js
@@ -0,0 +1,1 @@
+import{E as i,s as e,wa as t}from"./chunk-GRMDKWTS.js";var r=class o{static \u0275fac=function(m){return new(m||o)};static \u0275mod=i({type:o});static \u0275inj=e({imports:[t,t]})};export{r as a};
---
+++ streaming/build/chunk-RDDWBWIV.js
@@ -0,0 +1,1 @@
+import{r as i,t as o,ya as e}from"./chunk-GRMDKWTS.js";var n=class t{BASE_URL="https://dev.vniox.com/api";http=o(e);url(r=""){return`${this.BASE_URL}/${r}`}static \u0275fac=function(a){return new(a||t)};static \u0275prov=i({token:t,factory:t.\u0275fac,providedIn:"root"})};export{n as a};
